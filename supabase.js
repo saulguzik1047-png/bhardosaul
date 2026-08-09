e374b3bb932fbf3521f768b87c28bcadbedfb5c9
saulguzik1047-png <saulguzik1047@gmail.com>
2026-08-09 20:54:53 -0300

    Update Supabase credentials

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://veifigwusojkurqwodhk.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlaWZpZ3d1c29qa3VycXdvZGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMTA4OTksImV4cCI6MjEwMTc4Njg5OX0.QFm9cPf9c47G37oIiTrzsD19DC-XKD6aTEHbqcY_Azs';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://perjhxqgcdccmfyazubi.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlcmpoeHFnY2RjY21meWF6dWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5MDA1ODQsImV4cCI6MjA5OTQ3NjU4NH0.fADhYiAjHFWRvp30UHdS5my9ROkemKB2dGgYKPeGQWM';
 
 export const supabaseClient = supabaseUrl && supabaseAnonKey
   ? createClient(supabaseUrl, supabaseAnonKey)
