d992b7fbdd06b746c929444d7b706ef65bc0da31
saul <saulguzik1047@gmail.com>
2026-09-01 10:27:23 -0300

    Usa tabela organizada na auditoria

--- script.jsx
+++ script.jsx
@@ -2980,33 +2980,7 @@ function App() {
       )}
 
       {telaAtual === 'auditoria' && autenticado && (
-        <div className="single-container">
-          <h2>📋 Auditoria de Movimentações</h2>
-          <div className="wrapper-tabela-scroll">
-            <table className="tabela-padrao">
-              <thead>
-                <tr>
-                  <th>Data</th>
-                  <th>Tipo</th>
-                  <th>Operador</th>
-                  <th>Motivo</th>
-                  <th>Detalhes</th>
-                </tr>
-              </thead>
-              <tbody>
-                {logsAuditoria.map((log) => (
-                  <tr key={log.id}>
-                    <td>{new Date(log.data).toLocaleString()}</td>
-                    <td>{log.tipo}</td>
-                    <td>{log.operador}</td>
-                    <td>{log.motivo}</td>
-                    <td>{JSON.stringify(log.detalhes)}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <Auditoria logsAuditoria={logsAuditoria} />
       )}
 
       {usuarioEditando && (
